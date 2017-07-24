e9299751518e7b2939ffcd375346c4faaf6812b9
Ben <benhoughta@hotmail.com>
2017-07-24 10:26:26 +1000

    Started creating controller script.
    Moving to other computer. Need to save.

---
+++ Assets/Snake.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour {
+
+    public bool hop;
+    public float speed;
+
+    float leftRightTurn;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void PlayerInput()
+    {
+        float moveX = Input.GetAxis("Horizontal") * (speed / 2) * (Time.deltaTime);
+        float moveZ = Input.GetAxis("Vertical") * (speed) * (Time.deltaTime);
+    }
+}
