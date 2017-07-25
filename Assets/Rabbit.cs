7e9157a41f3dc700db3e5e39d1985dc1ec2cb76a
SAE\bHoughton <benhoughta@hotmail.com>
2017-07-25 19:34:24 +1000

    Screwed around with a couple of things.
    Added a material to a cube. Tried a couple of things for the Rabbit movement. Nothing working, only ideas.

--- Assets/Rabbit.cs
+++ Assets/Rabbit.cs
@@ -1,28 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+//using System.Diagnostics;
 using UnityEngine;
+
 
 public class Rabbit : MonoBehaviour {
 
-    public bool hop;
+    public float hop;
     public float speed;
+    //public Stopwatch timer;
 
     float leftRightTurn;
     Rigidbody rb;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         rb = GetComponent<Rigidbody>();
+        //timer = new Stopwatch();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void PlayerInput()
+	void Update ()
     {
-        float moveX = Input.GetAxis("Horizontal") * (speed / 2) * (Time.deltaTime);
+       // PlayerInputBasic();
+        RabbitMove();
+
+    }
+
+    void PlayerInputBasic()
+    {
+        float moveX = Input.GetAxis("Horizontal") * (speed) * (Time.deltaTime);
         float moveZ = Input.GetAxis("Vertical") * (speed) * (Time.deltaTime);
+        transform.Translate(0, 0, moveZ);
+        //Debug.Log(moveZ);
+        transform.Translate(moveX, 0, 0);
+    }
+
+    void Movement()
+    {
+        
+    }
+
+    void RabbitMove()
+    {
+        //was thinking that it could be like a presure point and you build up a meter instead
+        float moveZ = Input.GetAxis("Vertical") * (speed) * (Time.deltaTime);
+        bool holding = false;
+        if (moveZ > 0.03f)
+        {
+            holding = true;
+        }
+        else
+        holding = false;
+        Debug.Log(holding);
     }
 }
