8475d4afb670e85481a014326af09ed5eeed857b
Ben <benhoughta@hotmail.com>
2017-07-26 17:24:08 +1000

    Added code and some psudo
    Broken everything

--- Assets/Rabbit.cs
+++ Assets/Rabbit.cs
@@ -11,20 +11,24 @@ public class Rabbit : MonoBehaviour {
     //public Stopwatch timer;
 
     float leftRightTurn;
+    float rabbitMoveForward;
     Rigidbody rb;
 
 	// Use this for initialization
 	void Start ()
     {
         rb = GetComponent<Rigidbody>();
+        rabbitMoveForward = 3.0f;
         //timer = new Stopwatch();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-       // PlayerInputBasic();
+        // PlayerInputBasic();
         RabbitMove();
+        //float moveZ = Input.GetAxis("Vertical");
+        //Debug.Log(moveZ);
 
     }
 
@@ -45,14 +49,87 @@ public class Rabbit : MonoBehaviour {
     void RabbitMove()
     {
         //was thinking that it could be like a presure point and you build up a meter instead
-        float moveZ = Input.GetAxis("Vertical") * (speed) * (Time.deltaTime);
-        bool holding = false;
-        if (moveZ > 0.03f)
+        float moveZ = Input.GetAxis("Vertical");
+        bool flickBegin = false;
+        if (moveZ >= 0.03f)
         {
-            holding = true;
+            flickBegin = true;
+            while (flickBegin == true)
+            {
+                float time = 1;
+                float endTime = 5;
+                if (time < endTime)
+                {
+                    time += Time.deltaTime;
+                    Debug.Log(time);
+                }
+                flickBegin = false;
+                time = 1;
+            }
         }
         else
-        holding = false;
-        Debug.Log(holding);
+            flickBegin = false;
+        Debug.Log(flickBegin);
+
+
+        /*
+          * start:
+         * lastholding = false
+         * update:
+         * if not movemode
+         *      if holding 
+         *          increase rabbitmove by deltatime
+         *      end
+         *      if not holding and lastholding
+         *          movemode = true
+         *      end
+         *  else
+         *      decrease rabbitmove by deltatime
+         *      if rabbitmove<0
+         *          movemode=false
+         *          rabbitmove=0;
+         *      else
+         *          rigidbody.addforce(transform.forward)
+         *      end
+         *  end
+         *  lastholding=holding
+
+         
+         
+         
+         
+         
+          * start:
+         * lastholding = false
+         * currentmove=0
+         * startpos (vector3)
+         * endpos (VECTOR3)
+         * update:
+         * if not modemode
+         *      if holding 
+         *          add value to rabbitmove
+         *      end
+         *      if not holding and lastholding
+         *          movemode = true
+         *          currentmove = 0
+         *          startpos=transform.pos
+         *          endpos=startpos+rabbitmove*tranform.forward
+         *      end
+         *  else
+         *      increase currentmove
+         *      if currentmove>rabbitmove
+         *          movemode=false
+         *          rabbitmove=0;
+         *      else
+         *          transform.pos = lerp(startpos,endpos,currentmove/rabbitmove)
+         *      end
+         *  end
+         
+         
+         
+         
+         */
+
+
     }
 }
